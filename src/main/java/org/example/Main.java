248da4b953fc64711fd20c13644c4b217d45924d
Laptop <shikha.prasang@gmail.com>
2023-07-14 17:27:22 -0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+import org.testng.annotations.*;
+
+public class Main {
+
+     @BeforeTest
+     public void base(){
+
+     }
+     @Test
+     public void test() {
+     }
+
+     @AfterTest
+     public void teardown(){
+
+     }
+}
